f8227b1540baa71c102cca8576ae60a4cc30463f
vFlyingCowboy <103060762+vFlyingCowboy@users.noreply.github.com>
2022-11-01 21:25:21 -0700

    change wording

--- events/new member.js
+++ events/new member.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'guildMemberAdd',
     once: false,
     execute(member) {
-        member.guild.channels.cache.find(channel => channel.name === "welcome-room").send(`${member.user} Welcome to the party!`)
+        member.guild.channels.cache.find(channel => channel.name === "welcome-room").send(`${member.user} Welcome to the Server!`)
     }
 }
