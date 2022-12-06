a501630390f1b2aaf26ce5e668dd9cbe838310d9
vFlyingCowboy <103060762+vFlyingCowboy@users.noreply.github.com>
2022-12-05 20:44:11 -0800

    remove config.json because env exist

--- deploy-commands.js
+++ deploy-commands.js
@@ -2,7 +2,7 @@ const fs = require('node:fs');
 const path = require('node:path');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord.js');
-const { clientId, token } = require('./config.json');
+require('dotenv').config()
 
 const commands = [];
 const commandsPath = path.join(__dirname, 'commands');
@@ -14,8 +14,8 @@ for (const file of commandFiles) {
     commands.push(command.data.toJSON());
 }
 
-const rest = new REST({ version: '10' }).setToken(token);
+const rest = new REST({ version: '10' }).setToken(process.env.BOT_TOKEN);
 
-rest.put(Routes.applicationCommands(clientId), { body: commands })
+rest.put(Routes.applicationCommands(process.env.CLIENT_ID), { body: commands })
     .then(() => console.log('Successfully registered application commands.'))
     .catch(console.error);
